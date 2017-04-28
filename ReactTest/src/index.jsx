4136299b175ed98d9624bacbfea2fb4c7f443ede
Gmai <guilherme.maibashi@gmail.com>
2017-04-27 22:33:49 -0400

    all files

---
+++ ReactTest/src/index.jsx
@@ -0,0 +1,25 @@
+﻿import React from "react"
+import {render} from "react-dom"
+
+class HelloWorld extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+	render () {
+		return (
+			<p>Hello, {this.props.greetTarget}!</p>
+		);
+	}
+}
+
+render(
+	<div>
+		<HelloWorld greetTarget="Batman" />
+		<HelloWorld greetTarget="Iron Man" />
+		<HelloWorld greetTarget="Nicolas Cage" />
+		<HelloWorld greetTarget="Mega Man" />
+		<HelloWorld greetTarget="Bono" />
+		<HelloWorld greetTarget="Catwoman" />
+	</div>,
+	document.querySelector("#container")
+);
---
+++ ReactTest/webpack.config.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+
+module.exports = {
+	entry: "./src/index.jsx",
+	output: { filename: "./dist/bundle.js" },
+	module: {
+		loaders: [
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ["react", "es2015", "stage-3"],
+				}
+			}
+		]
+	}
+};
